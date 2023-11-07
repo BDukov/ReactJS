923de2ba5eb08397ccb52c7aab78fdede8ca3ea9
Borislav Dukov <borislav.dukov@icloud.com>
2023-11-07 08:54:57 +0200

    forms

--- 05. Forms/src/App.jsx
+++ 05. Forms/src/App.jsx
@@ -1,17 +1,20 @@
-
-import './App.css'
-import ControlledForm from './components/ControlledForm'
+import "./App.css";
+import ControlledForm from "./components/ControlledForm";
+import { useRef } from "react";
 // import UncontrolledForm from './components/UncontrolledForm'
 
 function App() {
-
+  const formRef = useRef();
 
   return (
     <>
-  {/* <UncontrolledForm /> */}
-  <ControlledForm />
+      {/* <UncontrolledForm /> */}
+      <ControlledForm formRef={formRef} />
+      <button type="button" onClick={() => formRef.current.requestSubmit()}>
+        Submit
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 05. Forms/src/components/ControlledForm.jsx
+++ 05. Forms/src/components/ControlledForm.jsx
@@ -1,37 +1,56 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
 const formInitialState = {
-    username: "",
-    password: "",
-    age: ""
-}
+  username: "",
+  password: "",
+  age: "",
+  gender: "Male",
+  swimming: false,
+  football: false,
+  voleyball: false,
+};
 
-export default function ControlledForm() {
+export default function ControlledForm({ formRef }) {
+  const [formValues, setFormValues] = useState(formInitialState);
+  const [ageError, setAgeError] = useState("");
 
-    const [formValues, setFormValues] = useState(formInitialState);
-
-
-const changeHandler = (e) => {
-    setFormValues(state => ({
-        ...state,
-        [e.target.name]: e.target.value
+  const changeHandler = (e) => {
+    setFormValues((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
     }));
     console.log(e.target.value);
     console.log(e.target.name);
-}
+  };
 
-const resetFormHandler = () => {
+  const resetFormHandler = () => {
     setFormValues(formInitialState);
-}
+  };
 
-const submitHandler = () => {
+  const submitHandler = () => {
     resetFormHandler();
-}
+  };
+
+  const onCheckboxChange = (e) => {
+    setFormValues((state) => ({
+      ...state,
+      [e.target.name]: e.target.checked,
+    }));
+  };
+  // eslint-disable-next-line no-unused-vars
+  const ageValidator = (e) => {
+    if (formValues.age < 0 || formValues.age > 120) {
+      setAgeError("Age must be between 0 and 120");
+    } else {
+        setAgeError("");
+    }
+  };
 
   return (
     <>
       <h1>Controlled Form</h1>
-      <form>
+      <form ref={formRef}>
         <div>
           <label htmlFor="username">Username:</label>
           <input
@@ -44,16 +63,73 @@ const submitHandler = () => {
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="text" name="password" id="password" value={formValues.password} onChange={changeHandler} />
+          <input
+            type="text"
+            name="password"
+            id="password"
+            value={formValues.password}
+            onChange={changeHandler}
+          />
         </div>
         <div>
           <label htmlFor="age">Age:</label>
-          <input type="number" name="age" id="password"  value={formValues.age} onChange={changeHandler}/>
+          <input
+            type="number"
+            name="age"
+            id="password"
+            value={formValues.age}
+            onChange={changeHandler}
+            onBlur={ageValidator}
+          />
+          {ageError && <p>{ageError}</p>}
+        </div>
+        <div>
+          <label htmlFor="gender">Gender </label>
+          <select
+            name="gender"
+            id="gender"
+            onChange={changeHandler}
+            value={formValues.gender}
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <div>
+          <h3>Hobbies</h3>
+          <label htmlFor="hobbies">Swimming</label>
+          <input
+            type="checkbox"
+            name="swimming"
+            id="swimming"
+            checked={formValues.swimming}
+            onChange={onCheckboxChange}
+          />
+          <label htmlFor="hobbies">Football</label>
+          <input
+            type="checkbox"
+            name="football"
+            id="football"
+            checked={formValues.football}
+            onChange={onCheckboxChange}
+          />
+          <label htmlFor="hobbies">Voleyball</label>
+          <input
+            type="checkbox"
+            name="voleyball"
+            id="voleyball"
+            checked={formValues.voleyball}
+            onChange={onCheckboxChange}
+          />
         </div>
         <div>
           {/* <input type="submit" value="Register"/> */}
-          <button type="button" onClick={resetFormHandler}>Reset</button>
-          <button type="button" onClick={submitHandler}>Register</button>
+          <button type="button" onClick={resetFormHandler}>
+            Reset
+          </button>
+          <button type="button" onClick={submitHandler}>
+            Register
+          </button>
         </div>
       </form>
     </>
